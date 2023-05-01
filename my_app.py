154a5893d4fba423c7cfe57d3390e83f8db44922
Blisk11 <72028370+Blisk11@users.noreply.github.com>
2023-05-01 13:25:26 +0200

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -23,6 +23,12 @@ hide_streamlit_style = """<style>
 footer {visibility: hidden;}
 </style>
 """
+p = Path(__file__).with_name('LOGO LMDO petit.jpeg')
+filename = p.absolute()
+#t1.title(filename)
+#/app/boina_dashboard2/LOGO LMDO petit.jpeg
+logo = Image.open(filename)
+st.image(logo)
 
 pwd = st.text_input("Password:", value="")
 
@@ -31,17 +37,12 @@ if pwd!= 'fractaldefou':
 else:
     st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
-    t1, t2 = st.columns((0.5,1)) 
+    #t1, t2 = st.columns((0.25,1)) 
 
-    p = Path(__file__).with_name('LOGO LMDO petit.jpeg')
-    filename = p.absolute()
-    #t1.title(filename)
-    #/app/boina_dashboard2/LOGO LMDO petit.jpeg
-    logo = Image.open(filename)
 
-    t1.image(logo)
-    t2.title("Dashboard demo: La maison de l'ostéopathie")
-
+    st.title("Dashboard demo: La maison de l'ostéopathie")
+    
+    
     df = pd.read_excel('dashboard_df.xlsx', engine='openpyxl')
     df_copy = df.copy()
 
@@ -52,27 +53,63 @@ else:
     with st.spinner('Updating Report...'):
 
         #Metrics setting and rendering
-        f1, f2 = st.columns((1,1))
-        DF_filter = f1.selectbox('Choisir ostéo', df['agenda'].value_counts().index.to_list() , help = 'Filtrer les données pour un ou tous les ostéos')
-        df_data = df[df['agenda']==DF_filter]
+        f1, f2= st.columns((1,1))
+        
+        DF_filter = f1.selectbox('Choisir ostéo', df['agenda'].value_counts().index.to_list() , help = 'Filtrer les données pour un ou tous les ostéos (pour les 2 tableaux ci-dessous)')              
 
+        df_data = df[(df['agenda']==DF_filter)]
+        
+        f1, f2= st.columns((1,1))        
+        DF_legend = f1.selectbox('Choisir légende du tableau ci-dessous', ['nom osteo', 'rdv_internet', 'age_bin', 'motif_du_rdv', 'fiche_trouvé', 'duree_du_rdv', 'civilite', 'distance_bin', 'nbs_rdv_bin', 'nouveau_patient',] , help = 'La légende est la couleur de barre du tableau ci-dessous')              
+
+        df_data = df[(df['agenda']==DF_filter)]        
         g1, g2 = st.columns((1,1))
         # bar chart
 
-        data = df_data.groupby(['nom osteo', 'year', 'month_number', 'month'])['index'].count().reset_index()
+        groupby_df_list = list(set([DF_legend, 'year', 'month_number', 'month']))
+        data = df_data.groupby(groupby_df_list)['index'].count().reset_index()
         data.sort_values(['year', 'month_number'], ascending = [True, True], inplace=True)
         #data['month'] = data['month'].astype(str).str.replace('-', '_')
         data.rename(columns = {'index':'nbs rdv'}, inplace=True)
 
-        fig = px.bar(data, x="month", y="nbs rdv", color="nom osteo", title="Nb de rdv par mois")   
+        fig = px.bar(data, x="month", y="nbs rdv", color= DF_legend, title="Nombre de rdv par mois")   
         g1.plotly_chart(fig, use_container_width=True)
 
+        #Metrics setting and rendering
+        f11, f22, f33 = st.columns((1,1,1))
+        
+        DF_filter1 = f11.selectbox('Choisir ostéo', df['agenda'].value_counts().index.to_list() , key = 2, help = 'Filtrer les données pour un ou tous les ostéos (pour les 2 tableaux ci-dessous)')
+        
+        quarter = f22.multiselect("Choisir les trimestres", df[(df['agenda']==DF_filter)]['trimestre'].sort_values().drop_duplicates().to_list(), df['trimestre'].sort_values().drop_duplicates().to_list(),)
+
+        feature = f33.selectbox("Choisir caractéristique à analyser", ['rdv_internet', 'age_bin', 'motif_du_rdv', 'fiche_trouvé', 'duree_du_rdv', 'civilite', 'distance_bin', 'nbs_rdv_bin', 'nouveau_patient',] , help = "Choisir l'axe des X du tableau de gauche")
+        
+
+        df_data = df[(df['agenda']==DF_filter1) & (df['trimestre'].isin(quarter))]
+        
+        g22, g_empty, g33 = st.columns((1, 0.2, 1))
+
+
         # Here we use a column with categorical data
+        fig = px.histogram(df_data.sort_values(feature), x=feature, color = 'nom osteo', title=feature)    
+        g22.plotly_chart(fig, use_container_width=True)
+        
+        fig = px.pie(df_data.sort_values(feature), values='rdv_compte', names= feature, title = feature)
+        fig.update_traces(textposition='inside', textinfo='percent+label')
+        g33.plotly_chart(fig,)
 
-        fig = px.histogram(df_data.sort_values('age_bin'), x="age_bin", color = 'nom osteo', title="Nb de rdv par groupe d'âge")    
-        g2.plotly_chart(fig, use_container_width=True)
-
-
+        ###
+#        "Pie chart by custom feature"
+#        s1, s2, s3, s4 = st.columns((1,1,1,1))
+#        feature = s1.selectbox('Choisir caractéristique', ['rdv_internet', 'age_bin', 'motif_du_rdv', 'fiche_trouvé', 'duree_du_rdv', 'civilite', 'distance_bin',   #'nbs_rdv_bin', 'nouveau_patient',] , help = 'Choisie la caractéristique à Analyser')
+#        DF_filter = s2.selectbox('Choisir ostéo ', df['agenda'].drop_duplicates().to_list() , help = 'Filtrer les données pour un ou tous les ostéos')
+#        df_data = df[df['agenda']==DF_filter].dropna(subset = [feature])
+        
+#        fig = px.pie(df_data.sort_values(feature), values='rdv_compte', names= feature, title = feature)
+#        fig.update_traces(textposition='inside', textinfo='percent+label')
+#        g3.plotly_chart(fig,)
+        ###
+        
         px_table = df_data.groupby(['nom osteo', 'departement'])['rdv_compte'].sum().reset_index().sort_values('rdv_compte',ascending=False)
         px_table = px_table.pivot(index = 'departement', columns = 'nom osteo', values = 'rdv_compte')
         # sort columns by osteo
@@ -109,16 +146,4 @@ else:
             t=0
             )
         )
-        st.plotly_chart(fig,)
-
-
-
-        "Pie chart by custom feature"
-        s1, s2, s3, s4 = st.columns((1,1,1,1))
-        feature = s1.selectbox('Choisir caractéristique', ['rdv_internet', 'age_bin', 'motif_du_rdv', 'fiche_trouvé', 'duree_du_rdv', 'civilite', 'distance_bin', 'nbs_rdv_bin', 'nouveau_patient',] , help = 'Choisie la caractéristique à Analyser')
-        DF_filter = s2.selectbox('Choisir ostéo ', df['agenda'].drop_duplicates().to_list() , help = 'Filtrer les données pour un ou tous les ostéos')
-        df_data = df[df['agenda']==DF_filter].dropna(subset = [feature])
-        
-        fig = px.pie(df_data.sort_values(feature), values='rdv_compte', names= feature, title = feature)
-        fig.update_traces(textposition='inside', textinfo='percent+label')
         st.plotly_chart(fig,)
